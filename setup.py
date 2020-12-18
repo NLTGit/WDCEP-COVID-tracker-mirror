ee368cd90ea0b9670c73a797df07b196a30200a8
daynan@gmail.com <daynan@gmail.com>
2020-12-17 22:02:58 -0500

    add pkg setup

---
+++ setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup, find_packages
+
+setup(name='WDCEP-COVID-tracker',
+      version='1.0',
+      author='Daynan Crull, New Light Technologies, Inc',
+      packages=find_packages())
